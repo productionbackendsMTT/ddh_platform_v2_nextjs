f8cafde62626323d1152ebf4b3d2f97004c7a3bc
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2025-05-26 10:25:40 +0530

    Refactor styling in hotGames component for improved layout consistency

--- src/components/games/hotGames.tsx
+++ src/components/games/hotGames.tsx
@@ -26,21 +26,20 @@ const HotGames = () => {
   return (
    !isSwiped && <Carousel
 
-      className="portrait:pt-[3vh]   portrait:px-[2vh] landscape:px-[2vw] landscape:pt-[3vw] bg-[url('/assets/images/hotgames_bg.png')] bg-cover bg-no-repeat bg-center portrait:w-[29vh] portrait:h-[26vh] landscape:w-[29vw] landscape:h-[26vw]"
+      className="portrait:pt-[2vh]   portrait:px-[2vh] landscape:px-[2vw] landscape:pt-[2vw] bg-[url('/assets/images/hotgames_bg.png')] bg-cover bg-no-repeat bg-center portrait:w-[29vh] portrait:h-[26vh] landscape:w-[29vw] landscape:h-[26vw]"
     >
       <Image src={'/assets/images/hot_games_text.png'} alt='hot_games_text' width={1000} height={1000} quality={100} className=' absolute portrait:top-[-2.2vh] landscape:top-[-2.2vw] left-[50%] translate-x-[-50%] portrait:w-[13vh] portrait:h-[5vh] landscape:w-[13vw] object-contain landscape:h-[5vw]' />
       <CarouselContent>
         {chunkedHotGames.map((chunk: any[], index: number) => (
           <CarouselItem
             key={index}
-            className="flex flex-wrap   portrait:gap-[.5vh] landscape:gap-[.5vw] items-center justify-between"
+            className="flex flex-wrap   portrait:gap-x-[1.2vw] portrait:gap-y-[.5vw] landscape:gap-x-[1.2vw] landscape:gap-y-[.5vw] items-center justify-center"
           >
             {chunk.map((game, gameIndex) => (
-              <div  key={gameIndex} className=''>
-
+              <div  key={gameIndex}>
                 <GameCard
-                  ImageClass={' portrait:rounded-[1.5vh] landscape:rounded-[1.5vw] '}
-                  StylesClass={'portrait:w-[6.5vh] portrait:h-[10vh]  landscape:w-[6.5vw] landscape:h-[10vw]   portrait:rounded-[1.6vh] landscape:rounded-[1.6vw]'}
+                  ImageClass={' portrait:rounded-[2vh] landscape:rounded-[2vw] portrait:py-[.2vh] landscape:py-[.2vw]'}
+                  StylesClass={'portrait:w-[7vh] portrait:h-[11.2vh]  landscape:w-[7vw] landscape:h-[11.2vw]   portrait:rounded-[1.6vh] landscape:rounded-[1.6vw]'}
                 />
               </div>
             ))}
